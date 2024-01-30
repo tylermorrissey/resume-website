63acd4826a4a86940b918a537464f498ad6fb8cb
tylermorrissey <tylerpmorrissey@gmail.com>
2024-01-30 00:36:29 -0500

    feat: copy email to clipboard w/ tooltip

--- src/components/ContactBlock.tsx
+++ src/components/ContactBlock.tsx
@@ -3,20 +3,52 @@ import "../assets/css/ContactBlock.css";
 import gmailLogo from "../assets/images/gmail-logo.png";
 import githubLogo from "../assets/images/github-logo.png";
 import linkedinLogo from "../assets/images/toppng.com-linkedin-icon-black-linkedin-logo-495x499.png";
+import { Tooltip, ClickAwayListener } from "@mui/material";
 
 function ContactBlock() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText("tylerpmorrissey@gmail.com");
+    handleTooltipOpen();
+  }
+
   return (
     <div className="contact-content">
       <div className="name">Tyler Morrissey</div>
       <div className="title">Software Engineer</div>
       <div className="logos">
         <div className="gmail">
-          <img
-            className="logo"
-            style={{ width: 25, height: 25 }}
-            src={gmailLogo}
-            alt="gmail.logo"
-          />
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <Tooltip
+              title="Copied to clipboard"
+              PopperProps={{
+                disablePortal: true,
+              }}
+              onClose={handleTooltipClose}
+              open={open}
+              disableFocusListener
+              disableHoverListener
+              disableTouchListener
+            >
+              <a className={"gmail-logo"} onClick={() => copyToClipboard()}>
+                <img
+                  className="logo"
+                  style={{ width: 25, height: 25 }}
+                  src={gmailLogo}
+                  alt="gmail.logo"
+                />
+              </a>
+            </Tooltip>
+          </ClickAwayListener>
         </div>
         <div className="github">
           <a href="https://github.com/tylermorrissey">
@@ -31,7 +63,7 @@ function ContactBlock() {
         <div className="linkedin">
           <a href="https://www.linkedin.com/in/tylermorrissey/">
             <img
-              className="logo"
+              className="linkedin-logo"
               style={{ width: 25, height: 25 }}
               src={linkedinLogo}
               alt="gmail.logo"
