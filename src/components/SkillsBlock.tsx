1715804cd4e243475f0464f59c9af8a43ec5eaf1
tylermorrissey <tylerpmorrissey@gmail.com>
2024-03-05 19:00:05 -0500

    feat: skills sections might actually be done? maybe resizing text for mobile?

--- src/components/SkillsBlock.tsx
+++ src/components/SkillsBlock.tsx
@@ -70,11 +70,15 @@ function SkillBlock() {
             </div>
             <div className={"about-me-text-content"}>
               <div className={"about-me-text-small"}>
-                What have I been up to? What have I been up to?What have I been
-                up to?What have I been up to?What have I been up to?What have I
-                been up to?What have I been up to?What have I been up to?What
-                have I been up to?What have I been up to?What have I been up
-                to?What have I been up to?
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                lacinia, quam viverra accumsan tristique, nunc lectus
+                pellentesque urna, in fringilla est erat ut ex. Quisque a diam
+                neque. Cras quis fringilla tellus. Maecenas eleifend cursus
+                tempor. Sed dignissim porttitor diam, sit amet porta magna
+                sodales ac. Vivamus cursus, nisl sed fermentum tempus, metus
+                tellus eleifend elit, at sagittis arcu eros sit amet ante.
+                Curabitur nec sem ipsum. Mauris eget orci lacinia arcu elementum
+                faucibus ut quis tellus.
               </div>
             </div>
           </div>
