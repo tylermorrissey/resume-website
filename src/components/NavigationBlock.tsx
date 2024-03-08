6f51fc44dcca9555e876f339741063c94cfe52aa
tylermorrissey <tylerpmorrissey@gmail.com>
2024-03-07 19:43:55 -0500

    feat: oopsie

--- src/components/NavigationBlock.tsx
+++ src/components/NavigationBlock.tsx
@@ -2,16 +2,6 @@ import React from "react";
 import "../assets/css/NavigationBlock.css";
 
 function NavigationBlock() {
-  function scrollTo(e: React.MouseEvent) {
-    e.preventDefault();
-    console.log(document.querySelector("#skills-block"));
-    // window.location.replace("/#skills-block");
-    window.scrollTo({
-      top: document.querySelector("#skills-block")?.scrollTop,
-      // behavior: "smooth",
-    });
-  }
-
   return (
     <div className="navbar">
       <div className={"left-nav"}>
